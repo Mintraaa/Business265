9b4cb6463e087b6a446fadb40b1c62655948319d
Mintraaa <111554748+Mintraaa@users.noreply.github.com>
2023-01-13 16:57:02 +0700

    Add files via upload

---
+++ Business265/selectbindParam.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Test SQL injection</title>
+    </head>
+    <body>
+    <h1>Test SQL injection</h1>
+    <form action="select_bindParam.php" method="GET">
+        <input type="text" placeholder="Enter Customer ID" name="CustomerID">
+        <br><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
